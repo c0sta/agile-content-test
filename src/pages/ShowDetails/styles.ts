2fc213157704b905cec798a15d7bf40987d12758
gabrielcmoura <gabriel.moura@gsw.com.br>
2022-04-10 20:53:42 -0300

    fix: changed grid template rows

--- src/pages/ShowDetails/styles.ts
+++ src/pages/ShowDetails/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const ShowDetails = styled.div`
   display: grid;
-  grid-template-rows: 70% 30%;
+  grid-template-rows: 60% 40%;
   width: 100%;
   height: 100%;
 `
