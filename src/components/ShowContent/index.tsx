4caf639f28431fdf27999be7fc49bceece758501
gabrielcmoura <gabriel.moura@gsw.com.br>
2022-04-10 10:54:35 -0300

    feat: show content component

---
+++ src/components/ShowContent/index.tsx
@@ -0,0 +1,12 @@
+import * as SC from "./styles";
+
+const ShowContent: React.FC = () => {
+  return (
+    <SC.ShowContent>
+      <div>Content and Background</div>
+      <div>Seasons tabs </div>
+    </SC.ShowContent>
+  );
+};
+
+export default ShowContent;
