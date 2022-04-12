8e5d596ce90c562d9cffb7f671e32ba37b3c7dc4
gabrielcmoura <gabriel.moura@gsw.com.br>
2022-04-11 21:54:52 -0300

    feat: feature Cast tab

--- src/components/ShowFooter/FooterTabs/Cast/styles.ts
+++ src/components/ShowFooter/FooterTabs/Cast/styles.ts
@@ -1,3 +1,34 @@
 import styled from 'styled-components'
 
-export const Cast = styled.div``
+export const CastTab = styled.div`
+  width: 100%;
+`
+
+export const CastList = styled.ul`
+  display: flex;
+  align-items: center;
+  gap: 16px;
+  overflow-x: auto;
+  height: 100%;
+  width: 100%;
+  padding: 16px 8px;
+`
+
+export const CastListItem = styled.li`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100px;
+  min-width: 200px;
+  background: #212123;
+
+  h1 {
+    font-size: 1rem;
+    color: #fafdfd;
+  }
+  h3 {
+    font-size: 0.8rem;
+    color: #aaa;
+  }
+`
