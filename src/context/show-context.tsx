fccc9b71a99f5cd686aeb52c3a23258825f78b3b
gabrielcmoura <gabriel.moura@gsw.com.br>
2022-04-13 18:01:36 -0300

    feat: added overlayLoading to context

--- src/context/show-context.tsx
+++ src/context/show-context.tsx
@@ -1,23 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Show } from '../interfaces'
 
 type ShowContext = {
   show: Show | null
   setShow: (show: Show) => void
+  setOverlayLoading: (isLoading: boolean) => void
+  overlayLoading: boolean
 }
 
 export const ShowContext = React.createContext<ShowContext>({} as ShowContext)
 
 const ShowContextProvider: React.FC = ({ children }) => {
   const [showContext, setShowContext] = React.useState<Show>({} as Show)
+  const [overlayLoading, setOverlayLoading] = useState(true)
 
   const setShow = (show: Show) => setShowContext(show)
+
+  const handleSetOverlayLoading = (isLoading: boolean) =>
+    setOverlayLoading(isLoading)
 
   return (
     <ShowContext.Provider
       value={{
         show: showContext,
         setShow,
+        setOverlayLoading: handleSetOverlayLoading,
+        overlayLoading,
       }}
     >
       {children}
