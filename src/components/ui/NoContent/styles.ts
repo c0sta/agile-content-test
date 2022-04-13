d6f634106b0c4ecc59a1137d36ffb76632142e23
gabrielcmoura <gabriel.moura@gsw.com.br>
2022-04-13 17:59:37 -0300

    chore: moved UI components to ui/ folder

---
+++ src/components/ui/NoContent/styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const NoContentContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`
+
+export const NoContentIcon = styled.img`
+  height: 128px;
+  width: 128px;
+`
+
+export const NoContentTitle = styled.h1`
+  font-size: 1.6rem;
+  color: #aaaa;
+`
---
+++ src/components/ui/Tabs/styles.ts
@@ -0,0 +1,47 @@
+import styled from 'styled-components'
+
+export const Tabs = styled.nav`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`
+interface TabProps {
+  active?: boolean
+  align?: 'left' | 'center' | 'right'
+  fullWidth?: boolean
+}
+
+export const TabsHeader = styled.header<TabProps>`
+  box-sizing: border-box;
+  display: flex;
+  gap: 32px;
+  width: 100%;
+  overflow-x: auto;
+  overflow-y: hidden;
+  min-height: 64px;
+  /* border-bottom: 2px solid #aaa; */
+`
+
+export const Tab = styled.button<TabProps>`
+  height: 100%;
+  width: ${props => (props.fullWidth ? '100%' : 'auto')};
+  text-align: ${props => props.align || 'left'};
+  background: none;
+  border: none;
+  border-bottom: ${props => (props.active ? '2px solid #2e754b' : '')};
+  color: ${props => (props.active ? '#fafdfd' : '#aaa')};
+  font-size: 1.3rem;
+  white-space: nowrap;
+  transition: all 0.2s ease-in-out;
+
+  &:hover {
+    cursor: pointer;
+  }
+`
+
+export const TabContent = styled.section`
+  display: flex;
+  padding: 32px 0px;
+  overflow-x: hidden;
+  overflow-y: auto;
+`
